1c9086d7b500f59fa96ec89b7e461bc35b17ec00
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-02-24 01:46:28 -0800

    Database testing

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const app = express()
-const path = require ('path');
+const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
+const Dest = require('./model/destinationModel.js');
 
 mongoose.connect('mongodb://localhost:27017/exploreamerica')
-.then(() => {
-    console.log('DB connection open')
-})
-.catch(err => {
-    console.log(err)
-})
+    .then(() => {
+        console.log('DB connection open')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'views');
 app.use(methodOverride('_method'))
 
-app.get('/destination', function (req, res){
+app.get('/destination', function (req, res) {
+    const dest1 = new Dest({ name: 'Santa Monica Pier', price: 15, description: 'Amusement Park near the beach', location: 'Santa Monica, California' })
+    dest1.save();
+    console.log(dest1)
     res.render('destination.ejs')
 })
 
--- model/destinationModel.js
+++ model/destinationModel.js
@@ -1,21 +1,33 @@
 const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/exploreamerica')
+.then(() => {
+    console.log('DB connection open')
+})
+.catch(err => {
+    console.log(err)
+})
+
 const destSchema = new mongoose.Schema({
-    name: {type : String,
+    name: {
+        type: String,
         required: true
     },
     price: {
-        type : Number,
-        required : true
+        type: Number,
+        required: true
     },
     description: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
-    location: { type: String,
-        required : true
+    location: {
+        type: String,
+        required: true
     }
 })
 
+
 const Dest = mongoose.model('Dest', destSchema)
 module.exports = Dest;
+
