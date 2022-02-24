e4550bcb5faf04a23046aa9b8ce90c0e2eb9bdee
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-02-24 00:36:08 -0800

    added home page

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require('express')
+const app = express()
+const path = require ('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'views');
+app.use(methodOverride('_method'))
+
+app.get('/destination', function (req, res){
+    res.render('destination.ejs')
+})
+
+app.listen(3500)
