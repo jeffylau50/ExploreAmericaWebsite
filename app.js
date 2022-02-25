4810e0f6db56ec08cb021951a846190c484c7468
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-02-24 18:15:56 -0800

    added basic delete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const Dest = require('./model/destinationModel.js');
 const { defaultMaxListeners } = require('events');
 const res = require('express/lib/response');
+const { findByIdAndDelete } = require('./model/destinationModel.js');
 
 mongoose.connect('mongodb://localhost:27017/exploreamerica')
     .then(() => {
@@ -23,7 +24,6 @@ app.use(methodOverride('_method'))
 
 app.get('/destination', async function (req, res) {
     let allDest = await Dest.find({});
-    console.log(allDest)
     res.render('destination.ejs', {allDest})
 })
 
@@ -35,6 +35,16 @@ app.post('/destination/new', async function (req, res){
     let {name, price, description, location} = req.body
     const p = new Dest({name, price, description, location})
     p.save().then(p => console.log(p)).catch(err => console.log(err))
+    res.redirect('/destination');
+})
+
+app.get('/destination/:id', async function (req, res){
+    let destDetail = await Dest.findById(req.params.id)
+    res.render('detail.ejs', {destDetail})
+})
+
+app.delete('/destination/:id/delete', async function (req, res){
+    await Dest.findByIdAndDelete(req.params.id)
     res.redirect('/destination');
 })
 
