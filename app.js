a5ed29473275cb28b7418a71925c0663862386a0
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-02-25 21:00:10 -0800

    basic error handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override')
 const Dest = require('./model/destinationModel.js');
 const ejsMate = require('ejs-mate')
+const catchasy = require('./tool/catchasy.js')
+const errorHan = require('./tool/error.js')
 
 mongoose.connect('mongodb://localhost:27017/exploreamerica')
     .then(() => {
@@ -21,41 +23,51 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'))
 
-app.get('/destination', async function (req, res) {
+app.get('/destination', catchasy(async function (req, res) {
     let allDest = await Dest.find({});
     res.render('destination.ejs', {allDest})
-})
+}))
 
 app.get('/destination/new', function (req, res){
     res.render('newForm.ejs')
 })
 
-app.post('/destination/new', async function (req, res){
+app.post('/destination/new', catchasy(async function (req, res){
     let {name, price, description, location, imageURL} = req.body
     const p = new Dest({name, price, description, location, imageURL})
     p.save().then(p => console.log(p)).catch(err => console.log(err))
     res.redirect('/destination');
-})
+}))
 
-app.get('/destination/:id', async function (req, res){
+app.get('/destination/:id', catchasy(async function (req, res){
     let destDetail = await Dest.findById(req.params.id)
     res.render('detail.ejs', {destDetail})
-})
+}))
 
-app.delete('/destination/:id/delete', async function (req, res){
+app.delete('/destination/:id/delete', catchasy(async function (req, res){
     await Dest.findByIdAndDelete(req.params.id)
     res.redirect('/destination');
-})
+}))
 
-app.get('/destination/:id/edit', async function (req, res){
+app.get('/destination/:id/edit', catchasy(async function (req, res){
     let destDetail = await Dest.findById(req.params.id)
     res.render('editForm.ejs', {destDetail})
-})
+}))
 
-app.patch('/destination/:id/edit', async function (req, res){
+app.patch('/destination/:id/edit', catchasy(async function (req, res){
     let {name, price, description, location, imageURL} = req.body
     await Dest.findByIdAndUpdate(req.params.id, {name, price, description, location, imageURL})
     res.redirect(`/destination/${req.params.id}`);
+}))
+
+app.all('*', (req, res, next) => {
+    next(new errorHan('Page Not Found :(', 404))
+})
+
+app.use((err, req, res, next) => {
+const {statusCode = 500} = err;
+if (!err.message) err.message = 'Something went Wrong :('
+res.status(statusCode).render('errorPage.ejs', {err})
 })
 
 app.listen(3500)
